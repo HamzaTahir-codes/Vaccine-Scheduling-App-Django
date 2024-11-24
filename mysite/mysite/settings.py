ea45f4ee62ca324912f78ab00bf767b06bf761c3
HamzaTahir-codes <m.hamza.codes@gmail.com>
2024-11-24 20:39:53 +0500

    Added new extension django_extensions

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'vaccine.apps.VaccineConfig',
     "user.apps.UserConfig",
     "center.apps.CenterConfig",
